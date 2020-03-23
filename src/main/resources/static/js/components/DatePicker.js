c8fa348afdf498f07019053f77bbb84b7cbf87f2
heshaowei <heshaowei@huacenter.com>
2020-03-23 16:04:09 +0800

    日期选择框优化

--- src/main/resources/static/js/components/DatePicker.js
+++ src/main/resources/static/js/components/DatePicker.js
@@ -34,10 +34,12 @@ Vue.component("m-date-picker", {
                     :label="label"
                     :placeholder="placeholder"
                     readonly
-                    @focus="input"
-                    :value="date"
+                    @click="input"
+                    v-model="date"
+                     :clearable="true"
             ></v-text-field>
             <v-date-picker style="position:fixed;z-index: 999;" v-if="showPicker" v-model="date"></v-date-picker>
+            <div v-if="showPicker" style="position: fixed;z-index: 998;top: 0;bottom: 0;left: 0;right: 0;" @click="showPicker = false"></div>
         </div>
      `
 });
